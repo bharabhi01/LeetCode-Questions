0f4d15c8921eb78f196a6ce63da947e4a77ac9a3
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-02-09 20:06:42 +0530

    Time: 3 ms (97.48%), Space: 9.1 MB (20.29%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int func(int i, int j, vector<vector<int>> &triangle, vector<vector<int>>&dp)
+    {
+        int n = triangle.size();
+        int m = triangle[0].size();
+        
+        if(i == n-1)
+            return triangle[i][j];
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int down = triangle[i][j] + func(i + 1, j, triangle, dp);
+        int diag = triangle[i][j] + func(i + 1, j + 1, triangle, dp);
+        
+        return dp[i][j] = min(down, diag);
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        int m = triangle[0].size();
+        
+        vector<vector<int>>dp(n, vector<int>(n, -1));
+        return func(0, 0, triangle, dp); 
+    }
+};
