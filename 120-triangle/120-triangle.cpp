a36e54beed8f4e14858eed6ba2823ada335563ac
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-02-09 20:23:13 +0530

    Time: 16 ms (12.44%), Space: 8.6 MB (62.60%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -4,22 +4,24 @@ public:
         int n = triangle.size();
         int m = triangle[0].size();
         
-        vector<vector<int>>dp(n, vector<int>(n, 0));
+        vector<int>front(n, 0), curr(n, 0);
         
         for(int j = 0; j < n; j++)
-            dp[n - 1][j] = triangle[n - 1][j];
+            front[j] = triangle[n - 1][j];
         
         for(int i = n -2; i >= 0; i--)
         {
             for(int j = i; j >= 0; j--)
             {
-                int down = triangle[i][j] + dp[i + 1][j];
-                int diag = triangle[i][j] + dp[i + 1][j + 1];
+                int down = triangle[i][j] + front[j];
+                int diag = triangle[i][j] + front[j + 1];
                 
-                dp[i][j] = min(down, diag);
+                curr[j] = min(down, diag);
             }
+            
+            front = curr;
         }
         
-        return dp[0][0];
+        return front[0];
     }
 };
