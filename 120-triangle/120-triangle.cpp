8d351ad3a18edc54464d5a75deb0677e1c907e12
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-02-09 20:21:02 +0530

    Time: 9 ms (40.89%), Space: 9 MB (31.12%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int func(int i, int j, vector<vector<int>> &triangle, vector<vector<int>>&dp)
-    {
-        int n = triangle.size();
-        int m = triangle[0].size();
-        
-        if(i == n-1)
-            return triangle[i][j];
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int down = triangle[i][j] + func(i + 1, j, triangle, dp);
-        int diag = triangle[i][j] + func(i + 1, j + 1, triangle, dp);
-        
-        return dp[i][j] = min(down, diag);
-    }
-    
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         int m = triangle[0].size();
         
-        vector<vector<int>>dp(n, vector<int>(n, -1));
-        return func(0, 0, triangle, dp); 
+        vector<vector<int>>dp(n, vector<int>(n, 0));
+        
+        for(int j = 0; j < n; j++)
+            dp[n - 1][j] = triangle[n - 1][j];
+        
+        for(int i = n -2; i >= 0; i--)
+        {
+            for(int j = i; j >= 0; j--)
+            {
+                int down = triangle[i][j] + dp[i + 1][j];
+                int diag = triangle[i][j] + dp[i + 1][j + 1];
+                
+                dp[i][j] = min(down, diag);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
