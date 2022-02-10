c46cc7812071d193ec69f3863e3083516c03cda1
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-02-10 20:57:24 +0530

    Time: 174 ms (13.27%), Space: 8.8 MB (97.59%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -4,17 +4,18 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         
-        vector<vector<vector<int>>>dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+        vector<vector<int>>front(m, vector<int>(m, 0));
+        vector<vector<int>>curr(m, vector<int>(m, 0));
         
         for(int j1 = 0; j1 < m; j1++)
         {
             for(int j2 = 0; j2 < m; j2++)
             {
                 if(j1 == j2)
-                    dp[n - 1][j1][j2] = grid[n - 1][j1];
+                    front[j1][j2] = grid[n - 1][j1];
                 
                 else
-                    dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+                    front[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
             }
         }
         
@@ -40,17 +41,19 @@ public:
                             if ((j1 + di < 0 || j1 + di >= m) || (j2 + dj < 0 || j2 + dj >= m))
                                 value += -1e9;
                             else
-                                value += dp[i + 1][j1 + di][j2 + dj];
+                                value += front[j1 + di][j2 + dj];
 
                             maxi = max(value, maxi);
                         }
                     }
                     
-                    dp[i][j1][j2] = maxi;
+                    curr[j1][j2] = maxi;
                 }
             }
+            
+            front = curr;
         }
         
-        return dp[0][0][m - 1];
+        return front[0][m - 1];
     }
 };
