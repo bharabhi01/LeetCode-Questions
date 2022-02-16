601dd6ab036de489bb9948f66d8a454507cacbfb
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-02-16 13:34:18 +0530

    Time: 88 ms (69.53%), Space: 45.6 MB (14.79%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int func(int ind, int T, vector<int>& coins, vector<vector<int>> &dp)
+    {
+        if(ind == 0)
+        {
+            if(T % coins[0] == 0)
+                return T / coins[0];
+            
+            return 1e9;
+        }
+        
+        if(dp[ind][T] != -1)
+            return dp[ind][T];
+        
+        int notTake = 0 + func(ind - 1, T, coins, dp);
+        int take = INT_MAX;
+        if(coins[ind] <= T)
+            take = 1 + func(ind, T - coins[ind], coins, dp);
+        
+        return dp[ind][T] = min(take, notTake);
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>>dp(n, vector<int>(amount + 1, -1));
+        
+        int ans = func(n - 1, amount, coins, dp);
+        
+        if(ans >= 1e9)
+            return -1;
+        
+        return ans;
+    }
+};
