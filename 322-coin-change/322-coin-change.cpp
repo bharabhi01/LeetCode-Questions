f698191cca3c89b9ffa94d6fcc43a4f3f288e87d
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-02-16 13:41:07 +0530

    Time: 218 ms (15.41%), Space: 45.1 MB (16.35%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    int func(int ind, int T, vector<int>& coins, vector<vector<int>> &dp)
-    {
-        if(ind == 0)
-        {
-            if(T % coins[0] == 0)
-                return T / coins[0];
-            
-            return 1e9;
-        }
-        
-        if(dp[ind][T] != -1)
-            return dp[ind][T];
-        
-        int notTake = 0 + func(ind - 1, T, coins, dp);
-        int take = INT_MAX;
-        if(coins[ind] <= T)
-            take = 1 + func(ind, T - coins[ind], coins, dp);
-        
-        return dp[ind][T] = min(take, notTake);
-    }
-    
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount + 1, -1));
+        vector<vector<int>>dp(n, vector<int>(amount + 1, 0));
         
-        int ans = func(n - 1, amount, coins, dp);
+        for(int T = 0; T <= amount; T++)
+        {
+            if(T % coins[0] == 0)
+                dp[0][T] = T / coins[0];
+            else
+                dp[0][T] = 1e9;
+        }
+        
+        for(int ind = 1; ind < n; ind++)
+        {
+            for(int T = 0; T <= amount; T++)
+            {
+                int notTake = 0 + dp[ind - 1][T];
+                
+                int take = INT_MAX;
+                if(coins[ind] <= T)
+                    take = 1 + dp[ind][T - coins[ind]];
+
+                dp[ind][T] = min(take, notTake);
+            }
+        }
+        
+        int ans = dp[n - 1][amount];
         
         if(ans >= 1e9)
             return -1;
