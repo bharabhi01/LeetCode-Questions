4b4e8125fc61275bc4a12afb58f1596e03923f92
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-02-17 19:52:32 +0530

    Time: 81 ms (18.44%), Space: 18.4 MB (32.99%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,26 +1,26 @@
 class Solution {
-public:
-    int func(int ind, int T, vector<int> &coins, vector<vector<int>> &dp)
-    {
-        if(ind == 0)
-            return (T % coins[0] == 0);
-        
-        if(dp[ind][T] != -1)
-            return dp[ind][T];
-        
-        int notTake = func(ind - 1, T, coins, dp);
-        
-        int take = 0;
-        if(coins[ind] <= T)
-            take = func(ind, T - coins[ind], coins, dp);
-                    
-        return dp[ind][T] = take + notTake;
-    }
-    
+public:    
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount + 1, -1));
+        vector<vector<int>>dp(n, vector<int>(amount + 1, 0));
         
-        return func(n - 1, amount, coins, dp);
+        for(int T = 0; T <= amount; T++)
+            dp[0][T] = T % coins[0] == 0;
+        
+        for(int ind = 1; ind < n; ind++)
+        {
+            for(int T = 0; T <= amount; T++)
+            {
+                int notTake = dp[ind - 1][T];
+        
+                int take = 0;
+                if(coins[ind] <= T)
+                    take = dp[ind][T - coins[ind]];
+
+                dp[ind][T] = take + notTake;
+            }
+        }
+        
+        return dp[n - 1][amount];
     }
 };
