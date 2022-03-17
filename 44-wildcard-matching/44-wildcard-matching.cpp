6403e509e239febfad579054ebc474af48c47e54
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-03-17 19:07:30 +0530

    Time: 75 ms (50.88%), Space: 15.1 MB (30.78%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:   
     vector<vector<char>> dp;
-bool solve(int i, int j, string& s, string& p) {
-	if(j==p.length()) return i==s.length();
-	if(dp[i][j]!=-1) return dp[i][j];
-	if(p[j]=='*') return dp[i][j]= solve(i,j+1,s,p)||(i<s.length() && solve(i+1,j,s,p));
-	if(i<s.length() && (p[j]=='?'|| s[i]==p[j])) return dp[i][j]=solve(i+1,j+1,s,p);
-	return dp[i][j]=0;
-}
+    
+    bool solve(int i, int j, string& s, string& p) 
+    {
+        if(j == p.length()) 
+            return i == s.length();
 
-bool isMatch(string s, string p) {
-	dp.resize(s.size()+1,vector<char>(p.size(),-1));
-	return solve(0,0,s,p);    
-}
+        if(dp[i][j] != -1) 
+            return dp[i][j];
+        
+        if(p[j] == '*') 
+            return dp[i][j] = solve(i, j + 1, s, p) || (i < s.length() && solve(i + 1, j, s, p));
+        
+        if(i < s.length() && (p[j] == '?'|| s[i] == p[j])) 
+            return dp[i][j] = solve(i + 1, j + 1, s, p);
+        
+        return dp[i][j] = 0;
+    }
+
+    bool isMatch(string s, string p) {
+        dp.resize(s.size() + 1, vector<char>(p.size(), -1));
+        
+        return solve(0, 0, s, p);    
+    }
 };
