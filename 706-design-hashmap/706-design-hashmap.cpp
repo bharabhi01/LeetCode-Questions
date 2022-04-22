41bc57c700231e461721467d39bbcf85e4435dd5
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-04-22 22:31:46 +0530

    Time: 177 ms (59.72%), Space: 208.2 MB (29.50%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -0,0 +1,30 @@
+class MyHashMap {
+public:
+    vector<int>arr;
+    MyHashMap() {
+        arr.resize(1000001, -1);
+    }
+    
+    void put(int key, int value) {
+        arr[key] = value;
+    }
+    
+    int get(int key) {
+        if(arr[key] != -1)
+            return arr[key];
+        else
+            return -1;
+    }
+    
+    void remove(int key) {
+        arr[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
