5fa5e79eae74278758dba2f9d5a93a5a3f7dcaf8
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-07-21 19:31:20 +0530

    Time: 5 ms (45.04%), Space: 6.4 MB (56.39%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int n = a.size(), m = b.size();
-        int i = n - 1, j = m - 1, carry = 0;
-        string ans = "";
+        int i = a.size() - 1, j = b.size()-1;
+        int sum = 0;
         
-        while(i >= 0 || j >= 0 || carry != 0)
+        while(i >= 0 && j >= 0)
         {
-            if(i >= 0)
-            {
-                carry += a[i] == '0' ? 0 : 1;
-                i--;
-            }
-            
-            if(j >= 0)
-            {
-                carry += b[j] == '0' ? 0 : 1;
-                j--;
-            }
-            
-            ans = ((carry % 2) == 0 ? "0" : "1") + ans;
-            carry /= 2;
+            sum += (a[i] - '0') + (b[j] - '0');
+            a[i] = char(sum % 2 + '0');
+            sum /= 2;
+            i--,j--;
         }
         
-        return ans;
+        while(i >= 0)
+        {
+            sum += (a[i] - '0');
+            a[i] = char(sum % 2 + '0');
+            sum /= 2;
+            i--;
+        }
+    
+        while(j >= 0)
+        {
+            sum += (b[j] - '0');
+            a = string(1, char(sum % 2 + '0')) + a;
+            sum /= 2;
+            j--;
+        }
+        
+        if(sum)
+            a = string(1, char(sum + '0')) + a;
+        
+        return a;
     }
 };
