c76a24bda98f751d45a103cf520a2aa9dd0797f0
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-07-25 18:45:12 +0530

    Time: 10 ms (64.99%), Space: 26.6 MB (86.39%) - LeetHub

--- 593-valid-square/593-valid-square.cpp
+++ 593-valid-square/593-valid-square.cpp
@@ -5,14 +5,14 @@ public:
     }
     
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        unordered_set<int>st;
-        
-        st.insert(getLength(p1, p2));
-        st.insert(getLength(p1, p3));
-        st.insert(getLength(p1, p4));
-        st.insert(getLength(p2, p3));
-        st.insert(getLength(p2, p4));
-        st.insert(getLength(p3, p4));
+        unordered_set<int>st ({
+            getLength(p1, p2),
+            getLength(p1, p3),
+            getLength(p1, p4),
+            getLength(p2, p3),
+            getLength(p2, p4),
+            getLength(p3, p4),
+        });
         
         return st.size() == 2 && !st.count(0);
     }
