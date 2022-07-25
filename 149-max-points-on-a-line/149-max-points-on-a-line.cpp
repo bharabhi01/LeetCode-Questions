22e882a174ad7bdbcf5a53201ef7d00e06e0cc07
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-07-25 17:41:39 +0530

    Time: 13 ms (99.88%), Space: 7.5 MB (83.38%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -1,43 +1,60 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        int n = points.size(), res = 0;
+        vector<double> s;
+        int ans = 0, c = 0, f = 0, u = 0;
+        int n = points.size();
         
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < n - 1; i++)
         {
-            unordered_map<double,int>mp;
-            int duplicate = 0;
-            double slope = 0.0;
-
-            for(int j = 0; j < n; j++)
+            for(int j = i + 1; j < n; j++)
             {
-                int x1 = points[i][0], x2 = points[j][0], y1 = points[i][1], y2 = points[j][1];
+                double y = points[i][1] - points[j][1];
+                double x = points[i][0] - points[j][0];
                 
-                int dy = y2 - y1, dx = x2 - x1;
-                
-                if(dy == 0 && dx == 0)
+                if(x == 0 || y == 0)
                 {
-                    duplicate++;
-                    continue;
+                    if(x == 0)
+                        f++;
+                    
+                    if(y == 0)
+                        u++;
                 }
-                
-                if(dx != 0)
-                    slope = (dy * 1.0) / dx;
                 else
-                    slope = INT_MAX;
-                
-                mp[slope]++;
+                    s.push_back(y / x);
             }
             
-            if(mp.size() == 0)
-                res = duplicate;
-            else
+            sort(s.begin(),s.end());
+            
+            for(int j = 1; j < s.size(); j++)
             {
-                for(auto slope: mp)
-                    res = max(res, slope.second + duplicate);
+                if(s[j] == s[j - 1])
+                    c++;
+                else
+                {
+                    if(c + 1 > ans)
+                        ans = c + 1;
+                    
+                    c = 0;
+                }
             }
+            
+            if(c + 1 > ans)
+                ans = c + 1;
+            
+            c = 0;
+            
+            if(f > ans)
+                ans = f;
+            
+            if(u > ans)
+                ans = u;
+            
+            s.clear();
+            f = 0;
+            u = 0;
         }
         
-        return res;
+        return ans + 1;
     }
 };
