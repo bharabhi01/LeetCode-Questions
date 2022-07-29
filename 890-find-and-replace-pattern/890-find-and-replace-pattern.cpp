9e65b4699b46ab754d6db482fe23f6bc66272a32
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-07-29 11:41:49 +0530

    Time: 2 ms (91.01%), Space: 7.9 MB (92.07%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
+    bool is_iso(string a,string b) {
+        int m = a.size();
+        int n = b.size();
+        
+        if(m != n)
+            return false;
+        
+        int mp1[256] = {0};
+        int mp2[256] = {0};
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(mp1[a[i]] == 0 && mp2[b[i]] == 0)
+            {
+                mp1[a[i]] = b[i];
+                mp2[b[i]] = a[i];
+            }
+            else if(mp1[a[i]] != b[i])
+                return false;
+        }
+        
+        return true;
+    }
+    
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         vector<string> ans;
         
-        for(auto x : words)
+        for(auto &x : words)
         {
-            if(x.length() == pattern.length())
-            {
+            if(is_iso(x, pattern))
                 ans.push_back(x);
-                unordered_map<char,char> mx;
-                unordered_map<char,char> mp;
-                
-                for(int i = 0; i < x.length(); i++)
-                {
-                    if(mx.find(x[i]) == mx.end() && mp.find(pattern[i]) != mp.end())
-                    {
-                        ans.pop_back();
-                        i = x.length();
-                    }
-                    else if(mx.find(x[i]) != mx.end() && mp.find(pattern[i]) == mp.end())
-                    {
-                        ans.pop_back();
-                        i = x.length();
-                    }
-                    else if(mx.find(x[i]) == mx.end() && mp.find(pattern[i]) == mp.end())
-                    {
-                        mx[x[i]] = pattern[i];
-                        mp[pattern[i]] = x[i];
-                    }
-                    else if(mx[x[i]] != pattern[i] || mp[pattern[i]] != x[i])
-                    {
-                        ans.pop_back();
-                        i = x.length();
-                    }
-                }
-            }
         }
         
         return ans;
