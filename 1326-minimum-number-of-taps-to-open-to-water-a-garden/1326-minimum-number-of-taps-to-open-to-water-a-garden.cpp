4ad6b2b499874bb37eb4ccd4a9ff1a28b38ff4f7
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-08-01 21:52:02 +0530

    Time: 31 ms (71.24%), Space: 14.8 MB (47.42%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        int size = ranges.size();
-        int min = 0, reach = 0, taps = 0;
+        vector<int> jumps(n + 1, -1);
         
-        while(reach < n)
+        for(int i = 0; i <= n; ++i) 
         {
-            for(int i = 0; i < size; i++)
-            {
-                if(i - ranges[i] <= min && i + ranges[i] > reach)
-                    reach = i + ranges[i];
-            }
-        
-            if(min == reach)
-                return -1;
-        
-            taps++;
-            min = reach;
+            int start = max(0, i - ranges[i]);
+            int end = min(n, i + ranges[i]);
+            
+            jumps[start] = max(jumps[start], end - start);
         }
         
-        return taps;
+        int currEnd = 0, farthest = 0, count = 0;
+        
+        for(int i = 0; i < n; ++i) 
+        {
+            if(i > farthest) 
+                return -1;
+            
+            farthest = max(farthest, i + jumps[i]);
+            
+            if(i == currEnd) 
+            {
+                count++;
+                currEnd = farthest;
+            }
+        }
+        
+        return farthest >= n ? count : -1;
     }
 };
