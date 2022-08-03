92fa9a88c68718f6e0eb86c1272c06caff67542e
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-08-03 22:04:26 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (60.81%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> result;
-        vector<int> path;
-        backtrack(result, path, 1, k, n);
-        return result;
-    }
-
-void backtrack(vector<vector<int>> &result, vector<int> &path, int start, int k, int target) {
-        if(target == 0 && k == 0)
+    void helper(int i, int n, int k, vector<vector<int>> &ans, vector<int> &sum) {
+        if(n == 0 && k == 0)
         {
-            result.push_back(path);
+            ans.push_back(sum);
             return;
         }
-
-        for(int i = start; i <= 10 - k && i <= target; i++)
+        
+        for(int j = i; j <= 10 - k && j <= n; j++)
         {
-            path.push_back(i);
-            backtrack(result, path, i + 1, k - 1, target - i);
-            path.pop_back();
+            sum.push_back(j);
+            helper(j + 1, n - j, k - 1, ans, sum);
+            sum.pop_back();
         }
+    }
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>ans;
+        vector<int>sum;
+        
+        helper(1, n, k, ans, sum);
+        
+        return ans;
     }
 };
