26f430ed091d1c3c43418e72de428f9c05a2b555
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-08-09 08:33:54 +0530

    Time: 29 ms (99.08%), Space: 9.3 MB (67.38%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
+    int mod= 1e9 + 7;
     
     int numFactoredBinaryTrees(vector<int>& arr) {
-        long ans = 0;
-        unordered_map<int, long> dp;
+        int n = arr.size();
+        int sum = 1;
+        unordered_map<int, long> mp;
         
         sort(arr.begin(), arr.end());
         
-        for (int i = 0; i < arr.size(); i++) 
-        {
-            dp[arr[i]] = 1;
-            for (int j = 0; j < i; j++)
-                if (arr[i] % arr[j] == 0)
-                    dp[arr[i]] = (dp[arr[i]] + dp[arr[j]] * dp[arr[i] / arr[j]]) % mod;
-            
-            ans = (ans + dp[arr[i]]) % mod;
-        }
+        for(int i = 0; i < arr.size(); i++)
+            mp[arr[i]] = 1;
         
-        return ans;
+        for(int i = 1; i < n; i++)
+        {
+            double x = sqrt(arr[i]);
+            
+            for(int j = 0; j < i && arr[j] <= x; j++)
+            {
+                if(arr[i] % arr[j] == 0)
+                {
+                    if(arr[j] == arr[i] / arr[j])
+                        mp[arr[i]] += 1 * mp[arr[j]] * mp[arr[j]];
+                    else if(mp.find(arr[i] / arr[j]) != mp.end())
+                        mp[arr[i]] += 2 * mp[arr[j]] * mp[arr[i] / arr[j]];
+                }
+            }
+            sum = (sum + mp[arr[i]]) % mod;
+        }
+    
+        return sum;
     }
 };
