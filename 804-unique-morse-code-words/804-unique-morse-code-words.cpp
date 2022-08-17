8f783de1165b12364d77575ab8287bf04dd92f02
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-08-17 12:21:33 +0530

    Time: 4 ms (71.25%), Space: 8.8 MB (16.15%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        vector<string> morse_code = {".-", "-..." , "-.-.", "-..", ".", "..-.", "--.", "....", "..",
-                                 ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
-                                 "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
-    
-        unordered_set<string> mp;
-    
-        for(auto word : words) 
+        int ans = 0;
+        unordered_map<char, string> hash = {{'a',".-"},{'b', "-..."},{ 'c', "-.-."},{ 'd', "-.."},
+                                            {'e', "."},{'f', "..-."},{'g', "--."},{'h', "...."},
+                                            {'i', ".."},{'j', ".---"},{'k', "-.-"},{'l', ".-.."},
+                                            {'m', "--"},{'n', "-."},{'o', "---"},{'p', ".--."},
+                                            {'q', "--.-"},{'r', ".-."},{'s', "..."},{'t', "-"},
+                                            {'u', "..-"},{'v', "...-"},{'w', ".--"} ,{'x', "-..-"},
+                                            {'y', "-.--"},{'z', "--.."}};
+        
+        unordered_set<string> uniq_strings;
+        
+        for (auto& tmp: words)
         {
-            string code = "";
+            string tmp_str;
             
-            for(auto ch : word)
-                code += morse_code[ch - 'a'];
+            for (auto& ch: tmp)
+            {
+                auto node_hash = hash.find(ch);
+                if (node_hash != hash.end())
+                    tmp_str += node_hash->second;
+            }
             
-            mp.insert(code);
+            if (uniq_strings.find(tmp_str) == uniq_strings.end())
+                uniq_strings.emplace(tmp_str);
         }
-
-        return mp.size();
+        
+        ans = uniq_strings.size();
+        
+        return ans;
     }
 };
