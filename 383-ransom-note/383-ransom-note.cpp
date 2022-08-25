c7f54dec327cb347b4a772f4bbd42283fdd9b3d6
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-08-25 11:06:25 +0530

    Time: 9 ms (96.53%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int map1[26] = {0};
-        int map2[26] = {0};
-        
-        for(char ch : ransomNote)
-            map1[ch - 97]++;
-        
-        for(char ch : magazine)
-            map2[ch - 97]++;
-        
-        for(int i = 0; i < 26;i++)
-            if(map2[i] < map1[i]) 
+        int map[26] = {0};
+        for (int i = 0; i < magazine.size(); i++)
+            map[magazine[i]-'a'] ++;
+
+        for(int i = 0; i < ransomNote.size(); i++)
+        {
+            if(map[ransomNote[i] - 'a'] > 0)
+                map[ransomNote[i]-'a'] --;
+            else
                 return false;
+        }
         
         return true;
     }
