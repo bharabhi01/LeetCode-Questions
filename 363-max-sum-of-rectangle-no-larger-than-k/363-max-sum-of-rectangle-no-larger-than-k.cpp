2490c4126e27c3f8b4713bf21edbf0e75ee6a3a0
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-08-28 00:09:28 +0530

    Time: 209 ms (99.74%), Space: 10.3 MB (99.22%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,45 +1,55 @@
 class Solution {
 public:
-    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
-        int r = matrix.size(), c = matrix[0].size();
-        
-        int prefix[r + 1][c + 1];
-        memset(prefix, 0, sizeof(prefix));
-        
-        for(int i = 1; i <= r; i++) 
-            prefix[i][1] = prefix[i - 1][1] + matrix[i - 1][0];
-        
-        for(int i = 1; i <= c; i++) 
-            prefix[1][i] = prefix[1][i - 1] + matrix[0][i - 1];
-        
-        for(int i = 2; i <= r; i++)
-        {
-            for(int j = 2; j <= c; j++)
-                prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] 
-                             - prefix[i - 1][j - 1] + matrix[i - 1][j - 1];
-        }
-        
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {   
         int ans = INT_MIN;
         
-        for (int x1 = 1; x1 <= r; x1++) 
+        int R = matrix.size();
+        int C = matrix[0].size();
+        int arr[R];
+        
+        for(int l = 0; l < C; ++l) 
         {
-			for (int y1 = 1; y1 <= c; y1++) 
+            memset(arr, 0, sizeof(arr));
+            
+            for(int r = l; r < C; ++r) 
             {
-				for (int x2 = x1; x2 <= r; x2++) 
+                for(int up = 0; up < R; ++up)
+                    arr[up] += matrix[up][r]; 
+    
+                int sum = 0, g = INT_MIN;
+                
+                for(int up = 0; up < R; ++up) 
                 {
-					for (int y2 = y1; y2 <= c; y2++) 
+                    sum = max(sum + arr[up], arr[up]);
+                    g = max(g, sum);
+                    
+                    if(g == k) 
+                        return k;
+                }
+                
+                if(g < k) 
+                    ans = max(ans, g);
+                else 
+                {
+                    for(int up1 = 0; up1 < R; ++up1) 
                     {
-						int sum = prefix[x2][y2] - prefix[x1 - 1][y2] 
-                                - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1];
-						
-                        if (sum <= k) 
-							ans = max(ans, sum);
-					}
-				}
-
-			}
-		}
-
+                        sum = 0;
+                        
+                        for(int up2 = up1; up2 < R; ++up2) 
+                        {
+                            sum += arr[up2];
+                            
+                            if(sum <= k) 
+                                ans = max(ans, sum);
+                        }
+                        
+                        if(ans == k) 
+                            return k;
+                    }
+                }
+            }
+        }
+        
         return ans;
     }
 };
